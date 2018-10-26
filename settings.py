6711d68b1a177d2baa7d35e1700f4391cb51a9c2
kokoa <kokoapooh@gmail.com>
2018-10-26 17:46:24 +1300

    re add access token

--- settings.py
+++ settings.py
@@ -13,8 +13,6 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 # LINE
-YOUR_CHANNEL_SECRET = os.environ.get("88f393d163712384bd5915f67fa0ce43
-")
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YFeK7yp+M2av4vsHzUZaueQAXJoUPCGeNylZ0aUXcssEthlppWt/169OKtCcMUW/CeDlQzRBAnxa4WtOIZxE7hi09fuXVV8Jmoq2dJvF7cZ9/ibso3F2d2eme6rYnJtilBDT9Ws16BtUpJ0q/lE4WQdB04t89/1O/w1cDnyilFU=
-")
+YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
 
