30bb1c4486b25410d4bb645e72fdb0eb43b7b198
kokoa <kokoapooh@gmail.com>
2018-10-26 17:23:39 +1300

    add access token and secret

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import os
+import settings
 from flask import Flask, request, abort
 
 from linebot import (
@@ -13,8 +14,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi(settings.YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(settings.YOUR_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
---
+++ settings.py
@@ -0,0 +1,19 @@
+import os
+from os.path import dirname, join
+
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# LINE
+YOUR_CHANNEL_SECRET = os.environ.get("from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# LINE
+YOUR_CHANNEL_SECRET = os.environ.get("13047782a44ff9936c027ab0ac576a9a")
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YFeK7yp+M2av4vsHzUZaueQAXJoUPCGeNylZ0aUXcssEthlppWt/169OKtCcMUW/CeDlQzRBAnxa4WtOIZxE7hi09fuXVV8Jmoq2dJvF7cZ9/ibso3F2d2eme6rYnJtilBDT9Ws16BtUpJ0q/lE4WQdB04t89/1O/w1cDnyilFU=
+")
+
