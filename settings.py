6a524819f1496111d9ac43826b60d122ab8e59c5
kokoa <kokoapooh@gmail.com>
2018-10-26 17:31:11 +1300

    re add channel secret

--- settings.py
+++ settings.py
@@ -13,7 +13,8 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 # LINE
-YOUR_CHANNEL_SECRET = os.environ.get("13047782a44ff9936c027ab0ac576a9a")
+YOUR_CHANNEL_SECRET = os.environ.get("88f393d163712384bd5915f67fa0ce43
+")
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YFeK7yp+M2av4vsHzUZaueQAXJoUPCGeNylZ0aUXcssEthlppWt/169OKtCcMUW/CeDlQzRBAnxa4WtOIZxE7hi09fuXVV8Jmoq2dJvF7cZ9/ibso3F2d2eme6rYnJtilBDT9Ws16BtUpJ0q/lE4WQdB04t89/1O/w1cDnyilFU=
 ")
 
