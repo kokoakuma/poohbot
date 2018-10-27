ee78ebfefe38f839501a68b0ca7d225ae1ebe7c5
kokoa <kokoapooh@gmail.com>
2018-10-27 20:30:23 +1300

    re

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
-YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
